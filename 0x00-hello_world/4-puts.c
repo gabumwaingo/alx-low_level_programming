256e7d5ecd26189596d9d08ac17851463f8e1212
gabumwaingo <gabumwaingo@gmail.com>
2022-12-08 12:46:03 -0800

    Betty bye

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *
  * Return : Always 0 (Success)
  */
-int main(void)
+int main(void) /* No arguments are to be passed */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
