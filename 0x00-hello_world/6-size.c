f443e0dfdbd33d9f8a6fadfae1b516e2c3f5645e
gabumwaingo <gabumwaingo@gmail.com>
2022-12-09 02:27:47 -0800

    Territory makes no nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,10 @@
  */
 int main(void)
 {
-	int i;
-	long int j;
-	long long int k;
-	char c;
-	float f;
-
-	printf("Size of int: %ld bytes \n", (unsigned long)sizeof(i));
-	printf("Size of long int: %ld bytes \n", (unsigned long)sizeof(j));
-	printf("Size of long long int: %ld bytes \n", (unsigned long)sizeof(k));
-	printf("Size of char: %ld byte \n", (unsigned long)sizeof(c));
-	printf("Size of float: %ld bytes \n", (unsigned long)sizeof(f));
+	printf("Size of int: %lu bytes \n", sizeof(int));
+	printf("Size of long int: %lu bytes \n", sizeof(long int));
+	printf("Size of long long int: %lu bytes \n", sizeof(long long int));
+	printf("Size of char: %lu byte \n", sizeof(char));
+	printf("Size of float: %lu bytes \n", sizeof(float));
 	return (0);
 }
