0de607f7a268dde1229fb7bd603fd95c1c60d671
gabumwaingo <gabumwaingo@gmail.com>
2022-12-09 14:50:01 -0800

    Numberz print with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(48 + x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
