aed121704ba8bb76064c8730fea25b62527570bb
gabumwaingo <gabumwaingo@gmail.com>
2022-12-12 11:40:09 -0800

    Unique one tens digit combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,14 @@ int main(void)
 	{
 		for (b = a + 1; b <= 9; b++)
 		{
-				putchar(a + '0');
-				putchar(b + '0');
-				if (a == 8 && b == 9)
-				continue;
-				putchar(',');
-				putchar(' ');
+			putchar(a + '0');
+			putchar(b + '0');
+
+			if (a == 8 && b == 9)
+			continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
