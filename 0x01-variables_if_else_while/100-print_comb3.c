0fac96cbcfec8d8e19e7fc0fc19e49fa37f0d91e
gabumwaingo <gabumwaingo@gmail.com>
2022-12-12 03:55:52 -0800

    List 1-89

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,16 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a < 9; a++)
+	for (a = 0; a < 9; a++)
 	{
-		for (b = a + '1'; b <= 9; b++)
+		for (b = a + 1; b <= 9; b++)
 		{
-			putchar(a + '0');
-			putchar(b + '0');
-
-			if (a < '8' && b < '9')
-			continue;
-
-			putchar(',');
-			putchar(' ');
+				putchar(a + '0');
+				putchar(b + '0');
+				if (a == 8 && b == 9)
+				continue;
+				putchar(',');
+				putchar(' ');
 		}
 	}
 	putchar('\n');
