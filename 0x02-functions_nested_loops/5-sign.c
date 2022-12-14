2253b36fbc37b56f89bd4421076e9c5739e29e15
gabumwaingo <gabumwaingo@gmail.com>
2022-12-14 04:48:33 -0800

    Print signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints + or - sign
+ * @n: the number that determines the sign to be printed
+ *
+ * Return: 1 if n > 0, 0 if n == 0, -1 if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
