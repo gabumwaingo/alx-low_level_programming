4d63955372aa19182d280d3ad78793fbd22deb93
gabumwaingo <gabumwaingo@gmail.com>
2022-12-14 13:47:35 -0800

    Fibonacci nos

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long  t1 = 0, t2 = 1, nextTerm;
+	int i;
+
+	for (i = 0; i <= 50; i++)
+	{
+		nextTerm = t1 + t2;
+		printf("%lu, ", nextTerm);
+
+		t1 =  t2;
+		t2 = nextTerm;
+	}
+	return (0);
+}
