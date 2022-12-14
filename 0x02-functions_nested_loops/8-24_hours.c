1852ba8f40ec35752d8b94cfa69e78f0dc1a7d9c
gabumwaingo <gabumwaingo@gmail.com>
2022-12-14 07:11:30 -0800

    Jacks 24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,27 +7,18 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int hours, min;
 
-	for (a = 0; a <= 2; a++)
+	for (hours = 0; hours <= 23; hours++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (min = 0; min <= 59; min++)
 		{
-			if ((a >= 1 && b >= 9) || (a <= 2 && b <= 3))
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(58);
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
