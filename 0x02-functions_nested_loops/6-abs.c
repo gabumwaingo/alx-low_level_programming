17f428c1b19b5265fffd45c33741cabbb0178115
gabumwaingo <gabumwaingo@gmail.com>
2022-12-14 05:16:03 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @i: is the integer whose value is computed
+ *
+ * Return: Absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0 || i <= 0)
+	{
+		return (i);
+	}
+}
