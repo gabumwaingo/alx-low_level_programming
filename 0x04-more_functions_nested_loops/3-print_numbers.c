10321d01313944c132765ff140fe2e79be3afdef
gabumwaingo <gabumwaingo@gmail.com>
2022-12-15 06:04:01 -0800

    List 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9
+ *
+ * Returns: void
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
