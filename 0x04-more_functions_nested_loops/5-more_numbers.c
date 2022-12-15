565baac0f293cb4bc702b549a04b2c7cfa196c7c
gabumwaingo <gabumwaingo@gmail.com>
2022-12-15 06:38:20 -0800

    1-14 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 x10
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
