163057339e40684450c6ee3274e8dcdf318c5cb8
gabumwaingo <gabumwaingo@gmail.com>
2022-12-15 00:51:08 -0800

    Testing + and -

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests the function that prints if an integer is + or -
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = -4;
+
+	positive_or_negative(i);
+
+	return (0);
+}
