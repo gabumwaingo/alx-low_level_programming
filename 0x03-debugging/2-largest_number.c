da39fad6f42ad243209d296ed67385cf47ba26d7
gabumwaingo <gabumwaingo@gmail.com>
2022-12-15 02:26:39 -0800

    Trial

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,15 +16,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > c && c > b)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if (b > c && c > a)
+	else if (b > c)
 	{
 		largest = b;
 	}
