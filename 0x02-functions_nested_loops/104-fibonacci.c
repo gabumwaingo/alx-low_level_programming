09e4ec4964edb3a3174e8adf17a9402800323788
gabumwaingo <gabumwaingo@gmail.com>
2022-12-15 12:36:55 -0800

    Fibonachi

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,8 +9,15 @@
  */
 int main(void)
 {
-	unsigned long int i, bef = 1, aft = 2, l = 1000000000;
-	unsigned long int bef1, bef2, aft1, aft2;
+	unsigned long int i;
+	unsigned long int bef = 1;
+       	unsigned long int aft = 2;
+       	unsigned long int l = 1000000000;
+
+	unsigned long int bef1;
+        unsigned long int bef2;
+       	unsigned long int aft1;
+       	unsigned long int aft2;
 
 	printf("%lu", bef);
 
