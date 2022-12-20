1532f10f51b3307f99016fc8461de6294f6dd83c
gabumwaingo <gabumwaingo@gmail.com>
2022-12-20 09:31:21 -0800

    Printing a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string on stdout
+ * @str: the string to be printed
+ *
+ * Return: the printed string
+ */
+void _puts(char *str)
+{
+	int a;
+	char b;
+
+	for (a = 0; str[a]; a++)
+	{
+	}
+	b = str[a];
+
+	_putchar(b);
+}
