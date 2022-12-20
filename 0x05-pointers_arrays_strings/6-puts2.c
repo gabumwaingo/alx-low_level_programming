d334dbdc7d50d4ac846e4969d0544f5f8a004dbf
gabumwaingo <gabumwaingo@gmail.com>
2022-12-20 13:34:32 -0800

    Value of twos

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: input string
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+}
