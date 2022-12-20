5991f1d789df1b125902a1506e5d521ad3184b4c
gabumwaingo <gabumwaingo@gmail.com>
2022-12-20 09:16:56 -0800

    My strlen func

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - it returns lenth of a string
+ * @s: variable containing the string
+ *
+ * Return: string lenth
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != 0; a++)
+	{
+	}
+	return (a);
+}
