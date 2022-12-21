cfd83165e7db503146ca75cddb16c20b537d5c66
gabumwaingo <gabumwaingo@gmail.com>
2022-12-21 12:07:36 -0800

    Copying n bytes into dest string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,12 +13,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int length, j;
 
-	length = n;
+	length = 0;
 	while (dest[length] != '\0')
 	{
 		++length;
 	}
-	for (j = n; src[j] != '\0'; ++j, ++length)
+	for (j = 0; j < n && src[j] != '\0'; ++j, ++length)
 	{
 		dest[length] = src[j];
 		dest[length] = '\0';
