71dcb58ff4b358ab38701cf37b87526c889a3f9d
gabumwaingo <gabumwaingo@gmail.com>
2022-12-21 11:39:58 -0800

    Specifying bytes to be copied

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two stringsbut uses a modt n bytes
+ * from src
+ * @dest: first input string
+ * @src: string to be connected to dest
+ * @n: the number of bytes to be copied
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int length, j;
+
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		++length;
+	}
+	if (j <= n)
+	{
+		for (j = 0; src[j] != '\0'; ++j, ++length)
+		{
+			dest[length] = src[j];
+			dest[length] = '\0';
+		}
+	}
+	return (dest);
+}
