77ee3892ca12bfce62d95c5092fdd90cb2666f21
gabumwaingo <gabumwaingo@gmail.com>
2022-12-22 06:24:06 -0800

    My copy string function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first strings
+ * @s2: second string
+ *
+ * Return: 0 when strings are equal
+ * above or below 0 when strings are not equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] && s2[i]; i++)
+	{
+		if (s1[i] == s2[i] || (s1[i] ^ 32) == s2[i])
+		{
+			continue;
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (s1[i] == s2[i])
+	{
+		return (0);
+	}
+	else if ((s1[i] | 32) < (s2[i] | 32))
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
