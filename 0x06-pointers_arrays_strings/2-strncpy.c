0fe8f7ea5166fbe075ec648d0e0a3f4ce65505b7
gabumwaingo <gabumwaingo@gmail.com>
2022-12-22 05:26:36 -0800

    trial3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	i = 0;
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	while (i < 0)
 	{
