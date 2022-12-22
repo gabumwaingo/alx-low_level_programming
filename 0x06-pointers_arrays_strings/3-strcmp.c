d2199220ac76071714977b7ffeed93e06245ee50
gabumwaingo <gabumwaingo@gmail.com>
2022-12-22 06:45:31 -0800

    Cpy cat function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,20 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] && s2[i]; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (s1[i] == s2[i] || (s1[i] ^ 32) == s2[i])
-			continue;
-		else
-		break;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-	if (s1[i] == s2[i])
-
-		return (0);
-
-	if ((s1[i] | 32) < (s2[i] | 32))
-
-		return (-1);
-	
-	return (1);
+	return (0);
 }
