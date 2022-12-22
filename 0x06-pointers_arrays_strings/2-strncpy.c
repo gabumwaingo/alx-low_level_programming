980d4a9dfee2205114f5576e65a9b4d96a9aeac0
gabumwaingo <gabumwaingo@gmail.com>
2022-12-22 01:15:43 -0800

    Copying function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies content of src string to dest
+ * @dest: string to have content put
+ * @src: string to be copied
+ * @n: specifies the number of bytes to be copied
+ *
+ * Return: a pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < 0)
+	{
+		dest[i++] = '\0';
+	}
+	return (dest);
+}
