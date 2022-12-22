8eb61c5e3a7f96b5c127a9b9e3f11ccb4fac3e74
gabumwaingo <gabumwaingo@gmail.com>
2022-12-22 09:39:33 -0800

    Fixing

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,22 +8,22 @@
  */
 char *cap_string(char *str)
 {
-	int i, j;
-	char b[] = " ,;.!?\"(){}";
+	int i = 0, j;
+	char b[] = " \t\n,;.!?\"(){}";
 
 
 	while (*(str + i))
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if ((*str + i) >= 'a' && *(str + i) <= 'z')
 		{
 			if (i == 0)
-				str[i] = str[i] - 'a' - 'A';
+				*(str + i) -= 'a' - 'A';
 			else
 			{
 				for (j = 0; j <= 12; j++)
 				{
 					if (b[j] == *(str + i - 1))
-						str[i] -= 'a' - 'A';
+						*(str + i) -= 'a' - 'A';
 				}
 			}
 		}
