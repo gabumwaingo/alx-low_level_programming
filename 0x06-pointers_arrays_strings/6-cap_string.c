214c30e8f1a9fe8bb9b7d08f774dae1ff44e00e0
gabumwaingo <gabumwaingo@gmail.com>
2022-12-22 10:02:54 -0800

    Breakthrough

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,16 +14,16 @@ char *cap_string(char *str)
 
 	while (*(str + i))
 	{
-		if ((*str + i) >= 'a' && *(str + i) <= 'z')
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
 		{
 			if (i == 0)
-				*(str + i) -= 'a' + 'A';
+				*(str + i) -= 'a' - 'A';
 			else
 			{
 				for (j = 0; j <= 12; j++)
 				{
 					if (b[j] == *(str + i - 1))
-						*(str + i) -= 'a' + 'A';
+						*(str + i) -= 'a' - 'A';
 				}
 			}
 		}
