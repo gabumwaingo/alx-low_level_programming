bdeec007d3205592f992d8e1715ad4eac20da9ab
gabumwaingo <gabumwaingo@gmail.com>
2022-12-22 09:28:39 -0800

    Bugzz

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,9 +14,9 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0)
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (str[i] >= 'a' && str[i] >= 'z')
+			if (i == 0)
 				str[i] = str[i] - 'a' - 'A';
 			else
 			{
