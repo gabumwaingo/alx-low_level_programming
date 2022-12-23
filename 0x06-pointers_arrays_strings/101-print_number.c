f7ebd3d65245cc0b2e66abd20d1b6e6c23dd5fa0
gabumwaingo <gabumwaingo@gmail.com>
2022-12-23 07:10:45 -0800

    Printing integer nos

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,18 @@
  */
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-	if (n / 10)
+	x = n;
+
+	if (x / 10)
 	{
-		print_number(n / 10);
+		print_number(x / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((x % 10) + '0');
 }
