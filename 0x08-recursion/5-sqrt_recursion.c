abe944040accf127aa10def1a7f0dee23f766a02
gabumwaingo <gabumwaingo@gmail.com>
2023-01-05 11:38:09 -0800

    Squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * evaluate function - used to get pure root numbers
+ * @i: input int
+ * @n: input int
+ * Return: squareroot
+ */
+int evaluate(int i, int n)
+{
+	if (n == 0 || n == 2)
+		return (n);
+
+	else if (i * i < n)
+		return (evaluate(i + 1, n));
+
+	else if (i * i == n)
+		return (i);
+
+	return (-1);
+
+	return (-1);
+}
+/**
+ * _sqrt_recursion -  returns the natural square root of a number
+ * @n: the input integer
+ * Return: the squareroot of n
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+
+	else
+		return (evaluate(i, n));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if input int is prime
+ * @n: input int
+ *
+ */
+int is_prime_number(int n)
+{
