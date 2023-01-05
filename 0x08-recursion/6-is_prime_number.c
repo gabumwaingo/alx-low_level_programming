5521244bb6bde119ba2a391de7cc2aee0584108f
gabumwaingo <gabumwaingo@gmail.com>
2023-01-05 11:58:06 -0800

    Prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,34 @@
 #include "main.h"
 
 /**
+ * prime - finds prime numbers
+ * @x: the number suspected to be prime
+ * @y: the number to confirm the suspicion
+ *
+ * Return: 1 if prime 0 if not
+ */
+int prime(int x, int y)
+{
+	if (x % y == 0)
+	{
+		if (x == y)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + prime(x, y + 1));
+}
+/**
  * is_prime_number - returns 1 if input int is prime
  * @n: input int
+ * Return: 1 if prime 0 if not
  *
  */
 int is_prime_number(int n)
 {
+	if (n <= 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (prime(n, 2));
+}
