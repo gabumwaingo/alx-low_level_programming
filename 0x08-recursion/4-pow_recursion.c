54946eb403e7e2072c37870714cc41c01a76e5c1
gabumwaingo <gabumwaingo@gmail.com>
2023-01-05 07:11:35 -0800

    Powers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raise power of an int to another int
+ * @x: the int to be raised to a power
+ * @y: the power
+ *
+ * Return: x raised to y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * (x, y - 1));
+}
+
