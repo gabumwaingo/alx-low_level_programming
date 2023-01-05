cd3ceef0f46659e7490105f26cf549934455caf1
gabumwaingo <gabumwaingo@gmail.com>
2023-01-05 04:06:06 -0800

    Puts func

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,15 +10,12 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	while (s[i])
+	if (s[i] == '\0')
 	{
-		if (s[i] == '\0')
-		{
-			_putchar('\n');
-			return;
-		}
-		i++;
+		_putchar('\n');
+		return;
 	}
 
-	_putchar(*s);
+	_putchar(s[i]);
+	_puts_recursion(s + 1);
 }
