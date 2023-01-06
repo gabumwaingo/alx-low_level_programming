a0f70f5b4d45a2ff4aa46f44914899846555f30b
gabumwaingo <gabumwaingo@gmail.com>
2023-01-06 13:22:05 -0800

    Wild comparison

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string thaat is a special character
+ * Return: 1 if identical 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
