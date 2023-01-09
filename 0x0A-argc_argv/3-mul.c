aca4a8b9ee7bf5a914707495356e5d5eb5088d17
gabumwaingo <gabumwaingo@gmail.com>
2023-01-09 14:47:32 -0800

    Multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prograns entry point
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: 0 or 1 if arguments are less than two
+ */
+int main(int argc, char *argv[])
+{
+	int j, i, mult = 0;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	j = atoi(argv[1]);
+	i = atoi(argv[2]);
+	mult = j * i;
+
+	printf("%d\n", mult);
+
+	return (0);
+}
+
