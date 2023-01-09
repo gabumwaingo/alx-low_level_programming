c2a7cb1950e0b813962e0f496d17e64259958b1a
gabumwaingo <gabumwaingo@gmail.com>
2023-01-09 13:51:45 -0800

    Argument display

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * @argc: the number of arguments
+ * @argv: the arguments in array format
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
