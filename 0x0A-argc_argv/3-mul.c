7c9a3a398c8650d9c4771c1369e84b78003f8c14
gabumwaingo <gabumwaingo@gmail.com>
2023-01-10 14:25:18 -0800

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,48 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-/**
- * _atoi - converts string to int
- * @s: the string to be converted
- * Return: the integer after conversion
- */
-int _atoi(char *s)
-{
-	int x, y, len, n, f, digits;
 
-	x = 0;
-	y = 0;
-	len = 0;
-	n  = 0;
-	f  = 0;
-	digits = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (x < len && f == 0)
-	{
-		if (s[x] == "-")
-			y++;
-
-		if (s[x] >= '0' && s[x] <= '9')
-		{
-			digit = s[x] - '0';
-			if (y % 2)
-				digit = -digit;
-			
-			n = n * 10 + digit;
-			f = 1;
-			if (s[x + 1] < '0' || s[x + 1} > '9')
-				break;
-
-			f = 0;
-		}
-		x++;
-	}
-	if (f == 0)
-		return (0);
-		
-		return (n);
-}
 /**
  * main - prograns entry point
  * @argc: the number of arguments
@@ -55,11 +13,11 @@ int main(int argc, char *argv[])
 
 	if (argc < 3 || argc > 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
-	j = _atoi(argv[1]);
-	i = _atoi(argv[2]);
+	j = atoi(argv[1]);
+	i = atoi(argv[2]);
 	mult = j * i;
 
 	printf("%d\n", mult);
