c02a6de6a0dbf6aaa54b100603bd6195bd5a1f6b
gabumwaingo <gabumwaingo@gmail.com>
2023-01-11 15:57:52 -0800

    Test 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_strdup - returns pointer to newly allocated
+ * mem space with str copy
+ * @str: the reference
+ *
+ * Return: pointer to the mem space
+ */
+char *_strdup(char *str)
+{
+	unsigned int i;
+	char *arr;
+
+	if (str == NULL)
+		return (NULL);
+	arr = malloc(sizeof(char) * strlen(str));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < strlen(str); i++)
+		arr[i] = str[i];
+
+	return (arr);
+	free(arr);
+}
+
+
