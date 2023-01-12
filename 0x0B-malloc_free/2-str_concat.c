8844b8d2c3ccc04cd83577a84a16b2bdfa9870f4
gabumwaingo <gabumwaingo@gmail.com>
2023-01-11 17:25:11 -0800

    Combination with new mem location

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ char *str_concat(char *s1, char *s2)
 	int len;
 	char *arr;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
 
 	arr = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
 	len = strlen(s1) + strlen(s2);
