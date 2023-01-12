b21d5a81791568d610ae6c4a3e1db1ce15693eb2
gabumwaingo <gabumwaingo@gmail.com>
2023-01-12 03:42:12 -0800

    Finale

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,31 +11,42 @@
 int main(int argc, char *argv[])
 {
 	int n, j;
+	char *k;
 	unsigned int i;
 
-	int cents[] = {25, 10, 5, 2, 1};
+	int cents[] = {25, 10, 5, 2};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	n = atoi(argv[1]);
-
+	n = strtol(argv[1], &k, 10);
 	j = 0;
-
-	while (n > 1)
+	
+	if (!*k)
 	{
-		for (i = 0; i < sizeof(cents[i]); i++)
+
+		while (n > 1)
 		{
-			if (n >= cents[i])
+			for (i = 0; i < sizeof(cents[i]); i++)
 			{
-				j = n / cents[i];
-				n = n % cents[i];
+				if (n >= cents[i])
+				{
+					j += n / cents[i];
+					n = n % cents[i];
+				}
 			}
 		}
-		j++;
+		if (n == 1)
+			j++;
 	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 	printf("%d\n", j);
 	return (0);
 }
