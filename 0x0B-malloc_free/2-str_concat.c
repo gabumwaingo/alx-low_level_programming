0e5d7241199e275fac9ff513cf347e7c847f3447
gabumwaingo <gabumwaingo@gmail.com>
2023-01-12 04:17:23 -0800

    Bugfix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	char *arr;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "\r";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "\r";
 
 	arr = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
 	len = strlen(s1) + strlen(s2);
