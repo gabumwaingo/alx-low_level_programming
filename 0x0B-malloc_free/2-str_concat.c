433f4603991f84d2d0e64ec31551e2b501c847c6
gabumwaingo <gabumwaingo@gmail.com>
2023-01-11 17:03:01 -0800

    Task 3

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *str_concat - concatenates two strings into a
+ * new memory location
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to new memory location
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, len;
+	char *arr;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	arr = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
+	len = strlen(s1) + strlen(s2);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < strlen(s1); i++)
+	{
+		arr[i] = s1[i];
+	}
+	for (j = 0; j < strlen(s2); j++, i++)
+	{
+		arr[i] = s2[j];
+	}
+	arr[len] = '\0';
+	return (arr);
+}
