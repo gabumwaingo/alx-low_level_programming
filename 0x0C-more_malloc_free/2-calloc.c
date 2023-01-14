72c77dcf47d154654f4e6babbe51e0a7e9d5bfd6
gabumwaingo <gabumwaingo@gmail.com>
2023-01-13 16:21:54 -0800

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
 
-	arr = calloc(nmemb, sizeof(char));
+	arr = malloc(nmemb * size);
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
