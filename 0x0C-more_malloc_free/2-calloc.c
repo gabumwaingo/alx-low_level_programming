115b2559074f40c673cf8943d1fad875de4a41cc
gabumwaingo <gabumwaingo@gmail.com>
2023-01-13 16:33:17 -0800

    The dynamic calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,10 +13,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *arr;
 	unsigned int a;
 
-	arr = malloc(nmemb * size);
-
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
+	arr = malloc(nmemb * size);
 
 	if (arr == NULL)
 		return (NULL);
