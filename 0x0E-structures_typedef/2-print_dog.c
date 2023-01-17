dc50a7a7d0353c06dc8d9668400bbeb1f1d4e6cb
gabumwaingo <gabumwaingo@gmail.com>
2023-01-17 12:26:22 -0800

    Task 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,12 +15,12 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		}
 		else
-			printf("Name: (nil)\\n");
+			printf("Name: (nil)\n");
 		printf("Age: %.6f\n", d->age);
 
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)\n)");
+			printf("Owner: (nil)\n");
 	}
 }
