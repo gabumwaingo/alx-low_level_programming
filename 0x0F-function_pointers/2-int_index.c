db05c96185d7c9d6e173f3ffb1e3b24b915aaa81
gabumwaingo <gabumwaingo@gmail.com>
2023-01-19 11:46:44 -0800

    Index searching

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,22 +10,19 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (size <= 0)
-		return (-1);
-
-	i = 0;
-	while (i < size)
+	if (size > 0)
 	{
-		if (array && cmp)
+		if (array != NULL && cmp != NULL)
 		{
-			if (cmp(array[i]) != 0)
-				return (i);
+			while (i < size)
+			{
+				if (cmp(array[i]) != 0)
+					return (i);
+				i++;
+			}
 		}
-
-		i++;
 	}
-	if (cmp(array[i]) == 0)
-		return (-1);
+	return (-1);
 }
