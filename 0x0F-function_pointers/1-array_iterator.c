da99dd26f16bfa52e49ef8e01e1d4b4cbe855c2d
gabumwaingo <gabumwaingo@gmail.com>
2023-01-19 10:52:01 -0800

    Task 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,8 +13,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	if (array && action)
-		return;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	{
+		i = 0;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
 }
