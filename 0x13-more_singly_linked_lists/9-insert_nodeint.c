2eecf259d52042bf5d40d92fab594c4b374969eb
gabumwaingo <gabumwaingo@gmail.com>
2023-02-02 09:33:59 -0800

    New stuff

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	i = 0;
-	while (i < idx)
+	while (i < idx && head != NULL)
 	{
 		temp = temp->next;
 		i++;
