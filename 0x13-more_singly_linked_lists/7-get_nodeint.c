ff7b4ae8892a5ed3b626496196af8dc9f22f8994
gabumwaingo <gabumwaingo@gmail.com>
2023-02-02 06:51:20 -0800

    Update 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -21,9 +21,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		temp = temp->next;
 		i++;
+	
+		if (temp == NULL)
+			return (NULL);
 	}
-	if (temp == NULL)
-		return (NULL);
 	return (temp);
 }
 
