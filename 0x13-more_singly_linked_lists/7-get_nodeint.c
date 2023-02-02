d5514474c6228efc3f8307142c90a36c52881856
gabumwaingo <gabumwaingo@gmail.com>
2023-02-02 04:24:52 -0800

    Task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index - returnsba node
+ * @index: position of the node
+ * @head: pointer to head
+ * Return: node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *temp;
+
+	temp = head;
+	
+	i = 0;
+	while (i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL)
+		return (NULL);
+	return (temp);
+}
+
