7e1f0337e9a94cde97211381bbd264cece4209a0
gabumwaingo <gabumwaingo@gmail.com>
2023-02-02 11:35:14 -0800

    Update task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,9 +16,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 
-	temp = *head;
 	if (idx != 0)
 	{
+		temp = *head;
 		for (i = 0; i < idx - 1 && temp != NULL; i++)
 		{
 			temp = temp->next;
@@ -27,10 +27,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 	}
 	new = malloc(sizeof(listint_t));
-	new->n = n;
 
 	if (new == NULL)
 		return (NULL);
+	new->n = n;
 
 	if (idx == 0)
 	{
