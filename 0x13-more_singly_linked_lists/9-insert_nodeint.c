58b0dffdae8f90a5a1d4d9a6d0b923a4c29fe84c
gabumwaingo <gabumwaingo@gmail.com>
2023-02-02 09:21:48 -0800

    Debugging

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,9 +20,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = temp->next;
 		i++;
+	
+		new->next = temp->next;
+		temp->next = new;
+		new->n = n;
 	}
-	new->next = temp->next;
-	temp->next = new;
-	new->n = n;
 	return (new);
 }
