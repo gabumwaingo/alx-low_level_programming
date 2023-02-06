4c6fe2d56eb16d5cf594029c32b6921fd2ce00ef
gabumwaingo <gabumwaingo@gmail.com>
2023-02-06 12:50:18 -0800

    Convert to binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,20 +12,18 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int val = 0;
 	int i = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-	for (; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] == '1' || b[i] == '0')
+		{
+			val <<= 1;
+			val += b[i] - '0';
+		}
+		else
 			return (0);
 	}
-	for (; b[i] != '\0'; i++)
-	{
-		val <<= 1;
-		if (b[i] == '1')
-		{
-			val += 1;
-		}
-	}
+
 	return (val);
 }
