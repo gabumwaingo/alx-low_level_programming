549d4b0d9879cd309d10957eeb059bd44a92d3bf
gabumwaingo <gabumwaingo@gmail.com>
2023-02-06 11:17:44 -0800

    Task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,15 +14,18 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	while (b[i] != '0' && b[i] != '1')
+	for (; b[i] != '\0'; i++)
 	{
-		i++;
-		return (0);
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
 	while (b[i] == '0' || b[i] == '1')
 	{
 		val <<= 1;
-		val += b[i] - '0';
+		if (b[i] == '1')
+		{
+			val += b[i] - '0';
+		}
 		i++;
 	}
 	return (val);
