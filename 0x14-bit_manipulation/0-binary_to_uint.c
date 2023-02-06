b4b9860e6a4c66bab850432724844b4c0cb28d19
gabumwaingo <gabumwaingo@gmail.com>
2023-02-06 07:59:18 -0800

    Convert binary to digit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *  binary_to_uint - convert binary to unsigned int
+ *  @b: pointer to string containing character reps
+ *  for binary
+ *  Return: the unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int val = 1;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '0' && b[i] != '1')
+	{
+		i++;
+		return (0);
+	}
+	while (b[i] == '0' || b[i] == '1')
+	{
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
+	}
+	return (val);
+}
