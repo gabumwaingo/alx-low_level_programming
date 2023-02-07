e4a2597068dfd7d5b40064d3433c8e213f2de470
gabumwaingo <gabumwaingo@gmail.com>
2023-02-07 12:18:13 -0800

    Crackme

---
+++ 0x14-bit_manipulation/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	n = get_bit(1024, 10);
+	printf("%d\n", n);
+	n = get_bit(98, 1);
+	printf("%d\n", n);
+	n = get_bit(1024, 0);
+	printf("%d\n", n);
+	return (0);
+}
