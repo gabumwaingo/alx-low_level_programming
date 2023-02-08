f10917ca2b2ed79ea41b462252c39adb98529461
gabumwaingo <gabumwaingo@gmail.com>
2023-02-08 14:23:28 -0800

    Task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
 /**
  * create_file - creates a file
  * @filename: name of file to create
- * @text-content: where to write the file
+ * @text_content: where to write the file
  * Return: 1 success -1 fail
  */
 int create_file(const char *filename, char *text_content)
