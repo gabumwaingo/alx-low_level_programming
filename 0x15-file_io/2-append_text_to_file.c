b7137045cd800f9823cea7f86be3cf339fb3479e
gabumwaingo <gabumwaingo@gmail.com>
2023-02-08 14:42:21 -0800

    Trial 1

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * append_text_to_file - appends text to file
+ * @filename: name of the file
+ * @text_content: the text to be appended
+ * Return: 1 success -1 fail
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len;
+
+	if (filename == NULL)
+		return (-1);
+	len = 0;
+	while (text_content[len] != '\0')
+	{
+		len++;
+	}
+	o = open(filename, O_RDWR | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+	return (1);
+}
