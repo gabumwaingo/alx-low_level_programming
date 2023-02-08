b583d25924257efee07e7b9513217390bda92988
gabumwaingo <gabumwaingo@gmail.com>
2023-02-08 12:28:52 -0800

    Task 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile -  reads text files and prints to stdout
+ * @filename: name of file to be read
+ * @letters: the number of letters it reads
+ * Return: number of letters it reads and prints
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *ch = NULL;
+
+	ch = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, ch, letters);
+	w = write(STDOUT_FILENO, ch, r);
+
+	if (0 == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(ch);
+		return (0);
+	}
+	free(ch);
+	close(o);
+	return (w);
+}
+
