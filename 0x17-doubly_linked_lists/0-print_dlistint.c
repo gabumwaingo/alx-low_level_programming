a510cac9781ace8c12682cadbed52e6e7a93903a
gabumwaingo <gabumwaingo@gmail.com>
2023-03-19 00:50:30 -0700

    Task 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints elements of dlistint_t
+ * @h: pointer to the the head of the list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}		
