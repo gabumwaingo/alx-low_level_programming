2191629b60919e60ba5b92fa7cb9e26d53244724
gabumwaingo <gabumwaingo@gmail.com>
2023-03-20 13:02:59 -0700

    Jackpot!

---
+++ 0x18-dynamic_libraries/_add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds teo ints
+ * @a: first int
+ * @b: second int
+ * Return: sum of the two
+ */
+int _add(int a, int b)
+{
+	int s;
+
+	s = a + b;
+	return (s);
+}
---
+++ 0x18-dynamic_libraries/_sub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * sub - subtracts two ints
+ * @a: first int
+ * @b: second int
+ * Return: subtraction of the two ints
+ */
+int sub(int a, int b)
+{
+	int sub;
+
+	s = a - b;
+	return (s);
+}
